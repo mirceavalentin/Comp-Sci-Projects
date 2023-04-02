0b942de105fd8571da7e1cd4e5081b40dbde914e
mirceavalentin <marinescumirceavalentin@gmail.com>
2023-04-02 13:28:08 +0200

    First project!

--- Interpreter Project/test.c
+++ Interpreter Project/test.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    printf("Salut, sunt Mircea.\n");
+
+    enum Company { GOOGLE, FACEBOOK, XEROX, YAHOO, EBAY, MICROSOFT };
+
+    enum Company firstCompany = XEROX;
+    printf("First company is: %d\n", firstCompany);
+
+    enum Company secondCompany = GOOGLE;
+    printf("Second company is: %d\n", secondCompany);
+
+    enum Company thirdCompany = EBAY;
+    printf("Third company is: %d\n", thirdCompany);
+
     return 0;
 }
