7fc75036295dc1157351e99c80d2aadbba4a913f
mirceavalentin <marinescumirceavalentin@gmail.com>
2023-05-17 00:05:39 +0200

    Better server implementation. Final.

--- Networked Applications/server.py
+++ Networked Applications/server.py
@@ -7,39 +7,36 @@ clients = {}
 
 
 def broadcast_message(sender, message):
-     """Send a message to all connected clients except the sender."""
+     # send a message to all connected clients except the sender
      for client_socket in clients.values():
          if client_socket != sender:
              client_socket.send(message.encode("utf-8"))
 
 
 def remove_client(client_socket):
-    """Remove a client from the list of connected clients."""
+    # remove a client from the list of connected clients
     username = [key for key, value in clients.items() if value == client_socket]
     if username:
         username = username[0]
         del clients[username]
         print(f"User '{username}' disconnected.")
 
+
 def recieve_complete_message(client_socket):
     message = ""
-    
     while True:
         try:
             incoming_chunk =  client_socket.recv(4096).decode("utf-8")
-            
             message += incoming_chunk 
-                        
             if not incoming_chunk or "\n" in incoming_chunk: 
                 break
-                        
         except ConnectionResetError:
             remove_client(client_socket)
             print("BAD-RQST-BODY\n")
             break
     return message
-            
-            
+
+# each connected client is handled in a separate thread
 def handle_client(client_socket):
     user_list = {}
     while True:
@@ -50,33 +47,28 @@ def handle_client(client_socket):
                 # No data received, client has disconnected
                 remove_client(client_socket)
                 break
-            
             header, body = None, None
             parts = message.split(" ", 1)
-            
             if parts:
                 header = parts[0]
                 if len(parts) > 1:
                     body = parts[1]
-            
             
             if not ("HELLO-FROM" in header or "LIST" in header or "SEND" in header):
                 print("BAD-RQST-HDR\n")
                 client_socket.send("BAD-RQST-HDR\n".encode("utf-8"))
                 break
             
-            
             if not body and not "LIST" in message:
                 print("BAD-RQST-BODY\n")
                 client_socket.send("BAD-RQST-BODY\n".encode("utf-8"))
                 break
-            
 
-            # Process the received message
+            # process the received message
             if message.startswith("HELLO-FROM"):
                 incoming_username = body
 
-                # Check if the username contains whitespace
+                # check if the username contains whitespace
                 if " " in incoming_username:
                     client_socket.send("BAD-RQST-BODY\n".encode("utf-8"))
                     remove_client(client_socket)
@@ -125,34 +117,26 @@ def handle_client(client_socket):
             break
 
 def start_server():
+    max_nr_clients = 2   # set to 2 for testing purposes
     """Start the chat server."""
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(("127.0.0.1", 1234))
-    server_socket.listen(65) # max 65 clients, >64 the minimum
     
     print("Chat server started.") 
 
     while True:
-        # Check if the maximum number of clients is reached
-        if len(clients) >= 65:
-            # Send "BUSY\n" message to the new client
-            client_socket, address = server_socket.accept()
+        server_socket.listen()
+        
+        client_socket, address = server_socket.accept()
+        
+        if len(clients) == max_nr_clients:
+        # Send "BUSY\n" message to the new client
             client_socket.send("BUSY\n".encode("utf-8"))
             client_socket.close()
-            continue
-        # Use select to check for incoming connections or data from clients
-        readable = select.select([server_socket] + list(clients.values()), [], [])[0]
-
-        for sock in readable:
-            if sock == server_socket:
-                # New connection, accept it and start a new thread to handle it
-                client_socket, address = server_socket.accept()
-                threading.Thread(target=handle_client, args=(client_socket,)).start()
-            else:
-                # Data available from a client, handle it in the same thread
-                handle_client(sock)
-
+        else:    
+            # New connection, accept it and start a new thread to handle it
+            threading.Thread(target=handle_client, args=(client_socket,)).start()
 
 if __name__ == "__main__":
     start_server()
